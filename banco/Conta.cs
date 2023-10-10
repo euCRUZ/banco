4609de79fb0a06d23dd1fb1da07f797a12f54a1c
Giovanni <giovannicruzvinhedo@gmail.com>
2023-10-09 21:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ banco/Conta.cs
@@ -0,0 +1,80 @@
+﻿using System.Transactions;
+
+class Conta
+{
+    // letra inicial maiúscula em propriedade pública
+    public int Numero { get; private set; } 
+    public string Titular { get; private set; }
+    public double Saldo { get; private set; }
+    public double? Limite { get; set; }
+
+    public Conta(int numero, string titular, double saldo, double limite)
+    {
+        Numero = numero;
+        Titular = titular;
+        Saldo = saldo;
+        Limite = limite;
+    }
+
+    public void Info()
+    {
+        string saldoFormatado = string.Format("{0:N2}", Saldo);
+        string limiteFormatado = string.Format("{0:N2}", Limite);
+
+        string output = "Nº da conta: " + Numero + "\n" +
+                        "Titular da conta: " + Titular + "\n" +
+                        "Saldo disponível: R$" + saldoFormatado + "\n" +
+                        "Limite da conta: R$" + limiteFormatado + "\n";
+
+        Console.WriteLine(output);
+    }
+
+    public void Extrato()
+    {
+        Console.WriteLine($"Saldo disponível do titular {Titular}: R${Saldo:N2}");
+    }
+
+    public void Deposita(double valor)
+    {
+        if (valor > 0)
+        {
+            Saldo += valor;
+        }
+        else
+        {
+            Console.WriteLine("O valor do depósito deve ser maior que R$0,00.");
+        }
+    }
+
+    private bool Pode_sacar(double valor_saque)
+    {
+        double vd = (double)(Saldo + Limite);
+        return valor_saque <= vd;
+    }
+
+    public void Saca(double valor)
+    {
+        if (Pode_sacar(valor))
+        {
+            Saldo -= valor;
+        }
+        else
+        {
+            Console.WriteLine($"O valor {valor:N2} passou o limite de saque.\n");
+        }
+    }
+
+    public void Envia(double valor, Conta destinatario)
+    {
+        if (Saldo >= valor && valor > 0)
+        {
+            Saca(valor);
+            destinatario.Deposita(valor);
+        }
+        else
+        {
+            Console.WriteLine($"O envio de {valor:N2} para {destinatario.Titular} não foi possível de ser efetuado.\n");
+        }
+    }
+
+}
---
+++ banco/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Conta c1 = new(1, "Giovanni", 580, 3000);
+        Conta c2 = new(2, "Saweetie", 600, 8000);
+
+        c1.Deposita(300);
+        c1.Saca(100);
+        c1.Envia(10, c2);
+        c1.Limite = 9000;
+
+        c1.Info();
+        c2.Info();
+    }
+}
